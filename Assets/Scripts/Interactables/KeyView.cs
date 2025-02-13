3648f4177d02c585216636afc880aa9bbf614f13
Sarangu Baisil Sanal <163407247+SaranguB@users.noreply.github.com>
2025-02-13 13:36:11 +0530

    Added UI Effect after RatRush

--- Assets/Scripts/Interactables/KeyView.cs
+++ Assets/Scripts/Interactables/KeyView.cs
@@ -9,6 +9,6 @@ public class KeyView : MonoBehaviour, IInteractable
         GameService.Instance.GetInstructionView().HideInstruction();
         GameService.Instance.GetSoundView().PlaySoundEffects(soundType);
         EventService.Instance.OnKeyPickedUp.InvokeEvent(++currentKeys);
-        gameObject.SetActive(false);
+        GetComponent<Collider>().enabled = false;
     }
 }
