6a0c09c6de337847c4d2dfb8eec1e0f10e88072f
Sarangu Baisil Sanal <163407247+SaranguB@users.noreply.github.com>
2025-02-13 10:55:33 +0530

    Implementation of generic Event Controller

--- Assets/Scripts/Service/EventService.cs
+++ Assets/Scripts/Service/EventService.cs
@@ -15,7 +15,7 @@ public class EventService
     }
 
     public EventController OnLightSwitchToggled { get; private set; }
-
+    public EventController<int> OnKeyPickedUP { get; private set; }
     public EventService()
     {
         OnLightSwitchToggled = new EventController();
